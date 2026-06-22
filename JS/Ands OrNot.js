0737bfb581843c9ed663f34d39df3284c2460585
pangolin <mayolc97@gmail.com>
2026-06-22 14:00:27 +0300

    Ands OrNot.js M

---
+++ JS/Ands OrNot.js
@@ -0,0 +1,55 @@
+/*
+
+comparison operators
+comparison operators evaluate only true or false
+
+*/
+
+
+/*
+And -> &&
+truth table -> all posible commbinations
+val1 and val2
+combinations are
+val1 && val2
+val2 && val1
+
+->for all statements have to be true for it to be true
+
+*/
+
+ang
+let val1=true;
+let val2=true;
+let result1=val1&&val2;
+
+console.log(`val1=${val1} val2=${val2} val1&&val2=${val1&&val2}`)
+
+let mercy=true;
+let mercy_balance=50000;
+
+//if mercy age is greater than 18 and has more than 20k in her account 
+let is_greater_18=mercy_age>18 //true or false
+console.log(`is mercy age greater than 18 ${is_greater_than_18}`)
+let has_more_than_20k=mercy_balance>20000
+console.log(`mercy has more than 20k ${has_more_than_20k}`)
+console.log(`mercy is age is greater than 18
+    and has more than 20k in her account ${is_greater_than_18&&has_more_than_20k}
+    `)
+    //mercy_age>18&mercy_balance>20000
+let simplified_age=mercy_age>18&&mercy_balance>20000
+console.log(`simplified ${simplified}`)
+console.log(`further simplication is ${mercy_age>18&&mercy_balance>20000}`)
+
+
+
+
+
+
+
+
+
+
+
+
+
