d8a36915b7ec20078006d4526e94951855ccf08e
pangolin <mayolc97@gmail.com>
2026-07-13 14:48:41 +0300

    introObject.js

---
+++ JS/intro/introObject.js
@@ -0,0 +1,61 @@
+
+/*
+
+
+
+
+Look at the differences
+between an array and an object.
+-›in an array keys are sequential<numbers›‹>
+
+8
+create an object that behaves Like an array.
+9
+1. create an array give it ‹any name› 10
+inside the array have two digits.
+e: ‹any digit›
+11
+1: ‹any digit›
+12
+2. create an object. ‹any name› 13
+inside the object have keys.
+14
+0: <any digit>
+15
+16
+1: ‹any digit>
+for the object create the Length key. 17
+have it as size of the object‹keys›
+18
+3. console.table both. you shoudl see there are 19
+the same.
+20
+*/
+21
+22
+23
+24
+25
+26
+27
+28
+29
+const ar = [20, 5];
+const arobj = {
+0: 20,
+1: 5,
+length: 2
+};
+console. table(ar);
+console. table(arobj) ;
+
+
+
+
+
+
+
+
+
+
+
